df2b14d82e34598d3847f6122f1e154e11f089ac
MuhammadQasim2006 <qasimmoos2006@gmail.com>
2026-05-25 10:18:52 +0200

    Add files via upload

---
+++ conditionals.js
@@ -0,0 +1,13 @@
+let score = 100
+
+const passinggrade = 60
+if (score >= passinggrade) {
+    console.log("Passed.")
+} else {
+    console.log("Failed.")
+}
+
+if (score === 100) {
+    console.log("Perfect score!")
+}
+// person whom i have worked with is Kirsten Layters //
